4373d20bf5ab607227a4e61020138e1c30148f95
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-08-06 22:27:31 +0530

    Update isPalindrome.js

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,3 +1,36 @@
+// Detect Palindrome -25:33:23
+// Description
+
+// Given an integer, print "Yes" (without quotes) if that integer is aPalindrome, else print "No" (without quotes)
+
+// You must write a recursive function to achieve this
+
+// You must not use the reverse() function. Using that would lead to disqualification
+
+
+// Input
+// Input Format
+
+// You are provided with one integer.
+
+// Constraints
+
+// The provided integer is always lesser than 1000000
+
+
+// Output
+// Print Yes or No depending upon the integer.
+
+
+// Sample Input 1 
+
+// 1221
+// Sample Output 1
+
+// Yes
+
+
+
 function isPalindrome(str){
     var N = str.length;
     var left = 0;
