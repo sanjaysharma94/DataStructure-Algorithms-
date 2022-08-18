021e1653d1014f9f109c2412fafc884d474af26b
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-08-18 22:00:17 +0530

    Create searchInSortedAndRotatedArray.js

---
+++ searchInSortedAndRotatedArray.js
@@ -0,0 +1,45 @@
+function runProgram(input) {
+    input = input.split("\n");
+    let [n,e] = input[0].split(" ").map(Number);
+    let line = 1;
+    let arr = input[line].split(" ").map(Number);
+    line++;
+    sort(n,e,arr);
+ }
+ 
+ function sort(n,e,arr){
+     let count=0
+     for(let i=0; i<n; i++){
+         if(arr[i]==e){
+             console.log(i)
+         }
+         else{
+            count++
+         }
+     }
+     if(count==n){
+         console.log(-1)
+     }
+ }
+ 
+ 
+ if (process.env.USERNAME === "") {
+     runProgram(``);
+ } else {
+     process.stdin.resume();
+     process.stdin.setEncoding("ascii");
+     let read = "";
+     process.stdin.on("data", function (input) {
+         read += input;
+     });
+     process.stdin.on("end", function () {
+         read = read.replace(/\n$/, "");
+         read = read.replace(/\n$/, "");
+         runProgram(read);
+     });
+     process.on("SIGINT", function () {
+         read = read.replace(/\n$/, "");
+         runProgram(read);
+         process.exit(0);
+     });
+ }
