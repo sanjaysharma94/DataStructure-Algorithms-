b0c98b50a0ea3f28785ada75a3180016ee96cccd
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-08-20 22:36:11 +0530

    Update ArrayPartsofFour.js

---
+++ ArrayPartsofFour.js
@@ -0,0 +1,13 @@
+function runProgram(input) {
+    input = input.split(/[\r\n]+/);
+    var N = +input[0];
+    var arr = input[1].trim().split(" ").map(Number);
+    
+    var n = N/4;
+    
+     var sum = 0
+    
+    for(let  i = 0; i<n; i++ ){
+        sum = sum + 2*arr[i]
+         
+    }
