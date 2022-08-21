55b81e0bde3519e293e398ed1b3f35573162cb09
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-08-21 21:43:00 +0530

    Update ElementInTheMiddle.js

--- ElementInTheMiddle.js
+++ ElementInTheMiddle.js
@@ -2,3 +2,11 @@ function runProgram(input) {
     input = input.split(/[\r\n]+/);
     var N = +input[0];
     var arr = input[1].trim().split(" ").map(Number);
+
+    for(var i = 1; i<N-1; i++){
+        if(arr[i-1]<arr[i] && arr[i+1]>arr[i]){
+            console.log(arr[i]);
+            break;
+        }
+    }
+  
