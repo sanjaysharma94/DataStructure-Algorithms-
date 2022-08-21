2154aa7bc5763c0e6cfaee7640aaaa7da75e291d
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-08-21 21:45:08 +0530

    Update ElementInTheMiddle.js

--- ElementInTheMiddle.js
+++ ElementInTheMiddle.js
@@ -27,3 +27,9 @@ if (process.env.USERNAME === "hp") {
     read = read.replace(/\n$/, "");
     runProgram(read);
   });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
