7737f204d907a9f628d645757e68ba67cfb60ad5
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-08-25 15:29:52 +0530

    Update raceDay.js

--- raceDay.js
+++ raceDay.js
@@ -15,3 +15,25 @@ function runProgram(input) {
         
         return ans;
     }
+
+}
+
+if (process.env.USERNAME === "hp") {
+  runProgram(`3
+1 2 3`);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
