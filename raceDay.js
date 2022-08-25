b7d79de59ee0bb9792c099e5c29b1b149b05b272
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-08-25 15:27:57 +0530

    Update raceDay.js

--- raceDay.js
+++ raceDay.js
@@ -5,3 +5,13 @@ function runProgram(input) {
         var arr = input[i].trim().split(" ").map(Number);
         console.log(raceDay(arr));
     }
+
+    function raceDay(arr){
+        let ans = 0;
+        
+        for(let i = 1; i<arr.length; i++){
+            if(arr[i]>arr[0]) ans++;
+        }
+        
+        return ans;
+    }
