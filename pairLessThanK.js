e60269e16951edc24cfd8fbeb2875e00d7bd1c58
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-08-28 20:21:41 +0530

    Update pairLessThanK.js

--- pairLessThanK.js
+++ pairLessThanK.js
@@ -31,4 +31,29 @@ function runProgram(input) {
         console.log(max);
         
     }
+
+    
+
+}
+
+if (process.env.USERNAME === "hp") {
+  runProgram(`3
+1 2 3`);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
     
