76cc8001773e4d95981b33e35136ff38f40fb384
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-08-28 12:20:51 +0530

    Update primeLoopCheck.js

--- primeLoopCheck.js
+++ primeLoopCheck.js
@@ -2,3 +2,11 @@ function primeLoopCheck(num) {
     for(var i = 2; i<=num; i++){
         var value = i;
         var count = 0;
+        for(var j = 2; j<value; j++){
+            if(value%j===0) count++;
+       }
+       
+       if(count===0) console.log(value); 
+       
+   }
+}
