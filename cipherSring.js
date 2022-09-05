ecdc832fd1e79b41e4069a8f4158d0136aac5a0e
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-09-05 23:02:01 +0530

    Update cipherSring.js

--- cipherSring.js
+++ cipherSring.js
@@ -45,3 +45,27 @@ function runProgram(input) {
     
 
 }
+
+
+}
+
+if (process.env.USERNAME === "hp") {
+  runProgram(`3
+1 2 3`);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
