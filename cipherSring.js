5f9d0962dc1656c09cbffd69bcb06115b9664c3a
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-09-05 22:59:36 +0530

    Update cipherSring.js

--- cipherSring.js
+++ cipherSring.js
@@ -9,3 +9,16 @@
 // output
 // a2b1c2
 // a2z1a2
+
+function runProgram(input) {
+    input = input.split(/[\r\n]+/);
+    var tc = +input[0];
+    var line = 1;
+    for(var i =0; i<tc; i++){
+      var N= +input[line];
+      line++;
+      var str = input[line]; 
+      line++;
+      
+      cipherstring(N,str)
+    }
